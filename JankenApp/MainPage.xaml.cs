d37bada08bfca444bdb90a39fa41602f92708cff
naka0303 <jyunki.nakagawa@gmail.com>
2022-09-11 15:35:57 +0900

    モジュール JankenApp の初期チェックイン

---
+++ JankenApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JankenApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonClicked(object sender, EventArgs e)
+        {
+            label.Text = "ボタン押下";
+        }
+    }
+}
